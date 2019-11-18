1020c8e5febc05b2f0d216d80151aa846bf679bd
yangjianbo <yangjianbo@daojia-inc.com>
2019-11-18 20:04:57 +0800

    commit

--- easy-mybatis/src/main/java/com/joaby/easy/study/mybatis/v2/executor/SimpleExecutorV2.java
+++ easy-mybatis/src/main/java/com/joaby/easy/study/mybatis/v2/executor/SimpleExecutorV2.java
@@ -13,20 +13,20 @@ public class SimpleExecutorV2 implements ExecutorV2 {
     public <T> T query(String statement, Object parameter, Class<T> resultType) {
         Connection conn = null;
         try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8", "root", "qq25616804");
-//            PreparedStatement psmt = conn.prepareStatement(statement);
-//            psmt.setInt(1, (Integer) parameter);
-//            ResultSet rs = psmt.executeQuery();
-//            Test test = null;
-//            while (rs.next()) {
-//                test = new Test();
-//                test.setId(rs.getInt(1));
-//                test.setName(rs.getString(2));
-//            }
-            Test test = new Test();
-            test.setId(1);
-            test.setName("JoabY");
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8", "root", "qq25616804");
+            PreparedStatement psmt = conn.prepareStatement(statement);
+            psmt.setInt(1, (Integer) parameter);
+            ResultSet rs = psmt.executeQuery();
+            Test test = null;
+            while (rs.next()) {
+                test = new Test();
+                test.setId(rs.getInt(1));
+                test.setName(rs.getString(2));
+            }
+//            Test test = new Test();
+//            test.setId(1);
+//            test.setName("JoabY");
             return (T)test;
         } catch (Exception e) {
             e.printStackTrace();
---
+++ easy-mybatis/src/main/java/com/joaby/easy/study/mybatis/v2/statement/ParameterHandlerV2.java
@@ -0,0 +1,10 @@
+package com.joaby.easy.study.mybatis.v2.statement;
+
+/**
+ * @author: yangjianbo
+ * @Date: Created in 2019/11/18 19:47
+ */
+public interface ParameterHandlerV2 {
+
+    <T> T query(String statement);
+}
